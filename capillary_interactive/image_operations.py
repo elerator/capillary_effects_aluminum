239828a98f8fb702917d949c40316d40ed5ed57a
Michael Gerstenberger <eler@gmx.net>
2019-10-18 14:47:04 +0200

    latest

--- capillary_interactive/image_operations.py
+++ capillary_interactive/image_operations.py
@@ -11,9 +11,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-
+from skimage import exposure
 from matplotlib import gridspec
 import matplotlib.pyplot as plt
+import matplotlib
+
+from collections import defaultdict
+from skimage.transform import (hough_line, hough_line_peaks, probabilistic_hough_line)
 
 def fig2rgb_array(fig):
     fig.canvas.draw()
@@ -21,14 +25,37 @@ def fig2rgb_array(fig):
     ncols, nrows = fig.canvas.get_width_height()
     return np.fromstring(buf, dtype=np.uint8).reshape(nrows, ncols, 3)
 
-def color_plot(image, plots = [], figsize=(12,8),dpi=200):
+def image_to_cascade_data(image):
+    data = defaultdict()
+    data["image_array"] = image
+    data["original"] = image.copy()
+    return data
+
+def color_plot(image, plot = None, figsize=(12,8),dpi=200, scatter=True, slope_intercept = None, markersize = 0.4):
     fig, ax = plt.subplots(figsize=figsize,dpi=dpi)
-    plt.tight_layout(pad=0)
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
+    plt.tight_layout(pad=0.8)
+    #ax.set_xticklabels([])#No labels for the ticks at the x axis
+    #ax.set_yticklabels([])
+    ax.set_yticks([x*50 for x in range(image.shape[1]//50)])
+    #ax.set_xticks([x*50 for x in range(image.shape[0]//50)])
+
+    ax.set_xlim((0, image.shape[1]))
+    ax.set_ylim((image.shape[0], 0))
+
+    plt.rcParams.update({'font.size': 12})
     ax.imshow(image, aspect="auto")
-    for p in plots:
-        ax.plot(p, color="red")
+
+    if type(plot)!=type(None):
+        if scatter:
+            ax.scatter(np.arange(len(plot)),plot, s = (plt.rcParams['lines.markersize']**2)*markersize,c = "red")
+        else:
+            ax.plot(plot, color="red")
+
+    if slope_intercept:
+        x = range(image.shape[1])#x values
+        for slope, intercept in slope_intercept:
+            ax.plot(x, intercept+x*slope, c = 'orange')
+
     return fig2rgb_array(fig)
 
 def load_docx(path):
@@ -67,11 +94,116 @@ def extract_pics_from_docx(path, target_path):
     shutil.rmtree("./TMP/")
     zip_ref.close()
 
-def crop(image_array,top,left,bottom,right):
-    image_array = image_array.copy()
-    return image_array[top:bottom,left:right]
+def crop(data,top,left,bottom,right):
+    image_array = data["image_array"]
+    data["image_array"] = image_array[top:bottom,left:right].copy()
+    data["cropped_original"] = image_array[top:bottom,left:right]
 
-def blur_columnwise(image_array, repetitions = 30, size=10):
+    return data
+
+def assign_columnwise(data, base_pos, min_columwidth = 5, pixels_per_micrometer=64):
+    points = data["1d_array"]#y values for x in range(0,len(points))
+    lines = data["houghlines"]
+
+    image_height = None
+    image_width = None
+    if "cropped_original" in data:
+        image_height = data["cropped_original"].shape[0]
+        image_width = data["cropped_original"].shape[1]
+    elif "original" in data:
+        image_height = data["original"].shape[0]
+        image_width = data["original"].shape[1]
+    else:
+        print("could not determin image_height")
+        return
+
+    if int(base_pos) < 0:
+        base_pos = image_height
+
+    #Find vertical borders of each column at a reasonable position
+    eval_y = np.nanmean(points)#evaluate x position for y coordinates
+    x_vals = [(eval_y - intercept)/slope for slope, intercept in lines]
+    x_vals.sort()
+
+    #Discard small distances...
+    min_columwidth = int(min_columwidth)
+    dists = [x2-x1 for x1, x2 in zip(x_vals,x_vals[1:])]
+    new_x_vals = []
+    merge_now = False
+    for i, _ in enumerate(dists):
+        if merge_now:
+            merge_now = False
+            continue
+        if dists[i] < min_columwidth:
+            prev_dist = None
+            next_dist = None
+            try:
+                prev_dist = dists[i-1]
+                next_dist = dists[i+1]
+            except:
+                pass
+            if prev_dist and next_dist:
+                if prev_dist < next_dist:#smaller column -> there is no border yet
+                    continue
+                else:#This is in fact the border but there is no border between next and the one after
+                    new_x_vals.append(x_vals[i])
+                    merge_now = True
+            else:
+                new_x_vals.append(x_vals[i])
+        else:
+            new_x_vals.append(x_vals[i])
+    x_vals = new_x_vals
+    x_vals.insert(0,0)
+    x_vals.append(image_width)
+
+    simplified = np.ndarray(len(points))
+    simplified.fill(0)
+    columnwise = []
+
+    #evaluate columnwise
+    for start, end in zip(x_vals, x_vals[1:]):
+        start = int(start)
+        end = int(end)
+        y_vals = points[start:end]
+        best_guess = np.nanmedian(y_vals)
+        columnwise.append(best_guess)
+        simplified[start:end] = best_guess
+    data["1d_array"] = simplified
+
+    data["columnwise"] = (image_height-np.array(columnwise)-(image_height-base_pos))/pixels_per_micrometer
+    del(data["houghlines"])
+
+    return data
+
+
+def detect_vertical_lines(data, max_deviation_from_vertical = 1, row_begin=100, row_end=-100, min_distance =50):
+    if "cropped_original" in data:
+        image = data["cropped_original"]
+    else:
+        image = data["original"]
+
+    image = np.gradient(image)[1]#Vertical component of gradient
+    image = image[row_begin:row_end,:]
+
+    h, theta, d = hough_line(image)#hough transform
+    h[:,:90-max_deviation_from_vertical] = 0 #Mask out areas that relate to non vertical lines
+    h[:,90+max_deviation_from_vertical:] = 0
+
+    h = np.log(1 + h) #log scale for better visibility
+
+    fx = []
+    for _, angle, dist in zip(*hough_line_peaks(h, theta, d, min_distance = min_distance)):
+        y0 = (dist                                 ) / np.sin(angle)#y for x = 0
+        y1 = (dist - image.shape[1] * np.cos(angle)) / np.sin(angle)#y for x = image.shape[1]
+        intercept = y0
+        slope = (y1-y0)/float(image.shape[1])
+        fx.append([slope, intercept])
+
+    data["houghlines"] = fx
+    #ax.plot(x, intercept+x*slope,  '-r')
+    return data
+
+def blur_columnwise(data, repetitions = 30, size=10):
     """ Blurs columnwise: Smoothing along dimension y.
     Args:
         image_array: The image to be processed. A 2d Numpy array.
@@ -80,14 +212,27 @@ def blur_columnwise(image_array, repetitions = 30, size=10):
     Returns:
         The filtered image.
     """
+    image_array = data["image_array"]
     image_array = image_array.copy()
     for repetition in range(repetitions):
         for x in range(image_array.shape[1]):#for each column: filter seperately
             image_array[:,x] = uniform_filter(image_array[:,x],10)
 
-    return image_array
+    data["image_array"] = image_array
+    return data
 
-def blur(image_array, repetitions = 10, size = 10):
+def equalize_histogram(data):
+    """
+        Histogram normalization
+    """
+    image_array = data["image_array"]
+    image_array = image_array.copy()
+    image_array = exposure.equalize_hist(image_array)
+    data["image_array"] = image_array
+    return data
+
+
+def blur(data, repetitions = 10, size = 10):
     """ Blurs image using uniform filtering.
     Args:
         image_array: The image to be processed. A 2d Numpy array.
@@ -96,25 +241,32 @@ def blur(image_array, repetitions = 10, size = 10):
     Returns:
         The filtered image.
     """
+    image_array = data["image_array"]
     image_array = image_array.copy()
 
     for x in range(repetitions):
         image_array = uniform_filter(image_array,10)
-    return image_array
+    data["image_array"] = image_array
+    return data
 
-def gradient_columnwise(image_array):
+def gradient_columnwise(data):
     """ Computes the gradient for each column.
     Args:
+        data: A
         image_array: The image to be processed. A 2d Numpy array.
     Returns:
         The filtered image.
     """
+    image_array = data["image_array"]
+
     image_array = image_array.copy()
     for x in range(image_array.shape[1]):
         image_array[:,x] = np.gradient(image_array[:,x])
-    return image_array
+    data["image_array"] = image_array
+    return data
 
-def gaussian_filter_nan(array, sigma):
+def gaussian_filter_nan(data, sigma):
+    array = data["1d_array"]
     U = np.asarray(array)
 
     if np.isnan(U[0]):
@@ -132,13 +284,19 @@ def gaussian_filter_nan(array, sigma):
 
     epsilon=0.000001
     Z=VV/(WW+epsilon)
-    return Z
+    data["1d_array"] = Z
+    return data
 
-def flip_ud(y,image_height):
-    return -y+ image_height
+def flip_ud(data,image_height):
+    y = data["1d_array"]
+    data["1d_array"] = -y+ image_height
+    return data
 
-def sample_via_threshold(image_array,start_from="top", threshold=.6, start = 500, shift = 0):
+def sample_via_threshold(data,start_from="top", threshold=.6, start = 500, shift = 0):
     """ Columnwise finds the first point where the image_array exceeds the threshold starting at start_from."""
+    image_array = data["image_array"]
+    if start < 0:
+        start = image_array.shape[1]+start
     ran = None
     if start_from == "top":
         ran = range(start,image_array.shape[0],1)#From top to bottom
@@ -156,28 +314,32 @@ def sample_via_threshold(image_array,start_from="top", threshold=.6, start = 500
                 break
         if not found:
             sampled_points.append(np.nan)
+    data["1d_array"] = sampled_points
+    return data
 
-    return sampled_points
-
-def horizontal_component(image_array):
+def horizontal_component(data):
     """ Retrieves horizontal component of gradient and standardizes the resulting values using the standard deviation"""
+    image_array = data["image_array"]
     horizontal_component = np.asarray(np.gradient(gaussian_filter(image_array,1))[0],dtype=np.float)
     horizontal_component = horizontal_component+np.abs(np.min(horizontal_component))
     horizontal_component /= np.std(horizontal_component)
-    return horizontal_component
+    data["image_array"] = horizontal_component
+    return data
 
 
-def remove_isolated_pixels(image_array, kernel_y=4, kernel_x=6):
+def remove_isolated_pixels(data, kernel_y=4, kernel_x=6):
     """ Processes a binary image and removes isolated pixel groups using the binary opening operator
     Args:
         image_array: 2D Numpy array containing binary values.
         kernel_y: Size of kernel for opening in dimension y
         kernel_x: Size of kernel for opening in dimension x
     """
+    image_array = data["image_array"]
     image_array = np.asarray(image_array,dtype=np.float)
     kernel = np.ones((kernel_y,kernel_x),np.uint8)
     opening = cv2.morphologyEx(image_array, cv2.MORPH_OPEN, kernel)
-    return opening
+    data["image_array"] = opening
+    return data
 
 def apply_cascade(data, cascade):
     """ Applies a cascade of image operations or filters one after the other and returns the final result
@@ -203,6 +365,9 @@ name_to_function["gradient columnwise"] = gradient_columnwise
 name_to_function["crop"] = crop
 name_to_function["horizontal component"] = horizontal_component
 name_to_function["remove isolated pixels"] = remove_isolated_pixels
+name_to_function["detect vertical lines"] = detect_vertical_lines
+name_to_function["equalize histogram"] = equalize_histogram
+name_to_function["assign columnwise"] = assign_columnwise
 
 function_to_default_params = {}
 function_to_default_params["crop"] = [200,20,-200,-20]
@@ -214,3 +379,6 @@ function_to_default_params["gaussian filter nan"]=[4]
 function_to_default_params["flip ud"] = []
 function_to_default_params["horizontal component"] = []
 function_to_default_params["remove isolated pixels"] = [4,6]
+function_to_default_params["detect vertical lines"] = [1,100,-100,20]
+function_to_default_params["equalize histogram"] =[]
+function_to_default_params["assign columnwise"] = [-1, 5]
--- capillary_interactive/kapilar.py
+++ capillary_interactive/kapilar.py
@@ -1,6 +1,6 @@
 from view import *
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QMessageBox
 from PyQt5.QtWidgets import QMainWindow, QFileDialog
 from PyQt5.QtCore import pyqtSignal, Qt, QModelIndex
 from PyQt5.QtGui import QStandardItemModel
@@ -14,6 +14,8 @@ from PyQt5.QtCore import QFile, QTextStream#Dark theme
 import breeze_resources
 import pickle
 from qevent_to_name import *
+
+import hashlib
 
 class FileDialog(QWidget):
     outfilepath = pyqtSignal(str)
@@ -59,24 +61,133 @@ class MainApp(QWidget):
         self.widget_handled = widget_handled #Widget for event Handling. All events are checked and if not arrow keys passed on. See eventFilter below
         QWidget.__init__(self, widget_handled)
 
-        self.model_2d_cascade = self.get_cascade_model()
-        assert type(self.model_2d_cascade) != type(None)
+        self.model_cascade_1 = self.get_cascade_model(self.ui.view_cascade_1,self.ui.combo_box_cascade_1)
+        self.model_cascade_2 = self.get_cascade_model(self.ui.view_cascade_2,self.ui.combo_box_cascade_2)
+
         self.files = []#list of filepaths
         self.tempfiles_path = os.getcwd()+"/tempfiles/"
 
-        self.cascade_outfile_dialog = FileDialog("pkl")
-        self.cascade_loadfile_dialog = FileDialog("pkl")
+        self.use_cropped = False
+
+        self.cascade_outfile_dialog_1 = FileDialog("pkl")
+        self.cascade_loadfile_dialog_1 = FileDialog("pkl")
+        self.cascade_outfile_dialog_2 = FileDialog("pkl")
+        self.cascade_loadfile_dialog_2 = FileDialog("pkl")
+
+        self.attributions_cascade_1 = {}
+        self.attributions_cascade_2 = {}
+
+        try:
+            with open(os.getcwd()+"/"+"attributions_cascade_1.ini", "rb") as f:
+                self.attributions_cascade_1 = pickle.load(f)
+        except Exception as e:
+            pass
+
+        try:
+            with open(os.getcwd()+"/"+"attributions_cascade_2.ini", "rb") as f:
+                self.attributions_cascade_2 = pickle.load(f)
+        except:
+            pass
+
+        self.use_attributions_cascade_1 = True
+        self.use_attributions_cascade_2 = True
 
         self.idx_image = 0
         self.make_connections()
 
 
+
     def shut_down(self):
-        try:
+        try:#Cleat temporary files
             shutil.rmtree(self.tempfiles_path)
         except:
             pass
+
+        try:
+            with open(os.getcwd()+"/"+"attributions_cascade_1.ini", "wb") as f:
+                pickle.dump(self.attributions_cascade_1, f)
+        except Exception as e:
+            QMessageBox.about(self, "Fatal error", "Couldn't save attributions\n" + str(e))
+            return
+
+        try:
+            with open(os.getcwd()+"/"+"attributions_cascade_2.ini", "wb") as f:
+                pickle.dump(self.attributions_cascade_2, f)
+        except Exception as e:
+            QMessageBox.about(self, "Fatal error", "Couldn't save attributions\n" + str(e))
+            return
         sys.exit()
+
+    """"def reload_cascade_attributions(self, filepath, cascade_no):
+        try:
+            with open(filepath, "rb") as f:
+                cascade = pickle.load(f)
+                if cascade_no == 1:
+                    self.attributions_cascade_1 = cascade
+                elif cascade_no ==2:
+                    self.attributions_cascade_2 = cascade
+        except:
+            print("Problem loading cascade attributions")
+            pass"""
+
+    def remember_attributions(self, cascade_no):
+        hash = self.get_hash()
+        if cascade_no == 1:
+            if not self.use_attributions_cascade_1:
+                return
+            self.attributions_cascade_1[hash]=self.cascade_model_to_list(self.model_cascade_1)
+        elif cascade_no == 2:
+            if not self.use_attributions_cascade_2:
+                return
+            self.attributions_cascade_2[hash]=self.cascade_model_to_list(self.model_cascade_2)
+        else:
+            raise Exception("No such cascade")
+
+
+    def get_hash(self):
+        """ Returns hash value for current image based on it's filename and a hash of the values"""
+        path = self.files[self.idx_image]
+        filename = path.split("/")[-1]
+        with open(path,"rb") as f:
+            hash_object = hashlib.sha512(f.read())
+            hex_dig = hash_object.hexdigest()
+        hash = filename + hex_dig
+        return hash
+
+    def update_model_via_attributions(self, cascade_no):
+        #Get correct attributions and check if work is necessary...
+        attributions = None
+        model = None
+        if cascade_no == 1:
+            attributions = self.attributions_cascade_1
+            model = self.model_cascade_1
+            if not self.use_attributions_cascade_1:
+                return
+        elif cascade_no == 2:
+            attributions = self.attributions_cascade_2
+            model = self.model_cascade_2
+            if not self.use_attributions_cascade_2:
+                return
+        else:
+            raise Exception("No such cascade")
+
+        try:
+            hash = self.get_hash()
+        except Exception as e:
+            print("Problem loading hash")
+            print(e)
+            return
+
+        if hash in attributions.keys():
+            cascade = attributions[hash]
+            model.removeRows(0,model.rowCount())
+            for row in cascade:
+                self.add_to_cascade_model(model, *row)#Add all items to model
+        else:
+            print(hash)
+            attributions.keys()
+
+
 
     def load_files(self, path):
         self.files = []#list of filepaths
@@ -91,6 +202,12 @@ class MainApp(QWidget):
             self.list_files(self.tempfiles_path, ".*\.png")
         self.show_image()
 
+        self.update_model_via_attributions(1)
+        self.update_model_via_attributions(2)
+
+    def toggle_use_crop(self, state):
+        self.use_cropped = state
+
     def list_files(self, path, regex):
         for f in os.scandir(path):
             if f.is_dir():
@@ -100,39 +217,65 @@ class MainApp(QWidget):
                     self.files.append(path+"/"+f.name)
 
     def make_connections(self):
+        self.ui.use_cropped.stateChanged.connect(self.toggle_use_crop)
         self.ui.next_image.clicked.connect(self.next_image)
         self.ui.previous_image.clicked.connect(self.previous_image)
-        self.ui.add_to_2d_cascade.clicked.connect(lambda: self.add_function_to_cascade(self.ui.combo_box_2d_cascade,self.model_2d_cascade))
-        self.ui.remove_from_2d_cascade.clicked.connect(lambda: self.remove_selected_from_model(self.model_2d_cascade, self.ui.view_2d_cascade))
-        self.ui.apply_cascade_2d.clicked.connect(self.apply_cascade)
-        self.ui.save_2d_cascade.clicked.connect(self.cascade_outfile_dialog.create_outputfile)
-        self.cascade_outfile_dialog.outfilepath.connect(lambda path: self.save_cascade_model(self.model_2d_cascade,path))
-        self.ui.load_2d_cascade.clicked.connect(self.cascade_loadfile_dialog.get_existing_file)
-        self.cascade_loadfile_dialog.filepath.connect(lambda path: self.load_cascade_model(self.model_2d_cascade,path))
+        self.ui.show_image.clicked.connect(lambda: self.set_current_index(self.idx_image))
 
-    def apply_cascade(self):
+        self.ui.add_to_cascade_1.clicked.connect(lambda: self.add_function_to_cascade(self.ui.combo_box_cascade_1,self.model_cascade_1))
+        self.ui.remove_from_cascade_1.clicked.connect(lambda: self.remove_selected_from_model(self.model_cascade_1, self.ui.view_cascade_1))
+        self.ui.apply_cascade_1.clicked.connect(lambda: self.apply_cascade(self.model_cascade_1,self.ui.error_cascade_1))
+        self.ui.save_cascade_1.clicked.connect(self.cascade_outfile_dialog_1.create_outputfile)
+        self.cascade_outfile_dialog_1.outfilepath.connect(lambda path: self.save_cascade_model(self.model_cascade_1,path))
+        self.ui.load_cascade_1.clicked.connect(self.cascade_loadfile_dialog_1.get_existing_file)
+        self.cascade_loadfile_dialog_1.filepath.connect(lambda path: self.load_cascade_model(self.model_cascade_1,path))
+
+        self.ui.add_to_cascade_2.clicked.connect(lambda: self.add_function_to_cascade(self.ui.combo_box_cascade_2,self.model_cascade_2))
+        self.ui.remove_from_cascade_2.clicked.connect(lambda: self.remove_selected_from_model(self.model_cascade_2, self.ui.view_cascade_2))
+        self.ui.apply_cascade_2.clicked.connect(lambda: self.apply_cascade(self.model_cascade_2,self.ui.error_cascade_2))
+        self.ui.save_cascade_2.clicked.connect(self.cascade_outfile_dialog_1.create_outputfile)
+        self.cascade_outfile_dialog_2.outfilepath.connect(lambda path: self.save_cascade_model(self.model_cascade_2,path))
+        self.ui.load_cascade_2.clicked.connect(self.cascade_loadfile_dialog_2.get_existing_file)
+        self.cascade_loadfile_dialog_2.filepath.connect(lambda path: self.load_cascade_model(self.model_cascade_2,path))
+
+    def apply_cascade(self, model, error_view):
         img = None
         try:
             img = Image.open(self.files[self.idx_image])
-            img = img.convert('RGB')
+            img = np.array(img.convert('I'), dtype=np.double)#Convert to grayscale
         except:
             return
 
-        operations = self.cascade_model_to_list( self.model_2d_cascade)
-        data = np.array(img, dtype=np.double)[:,:,0]
+        operations = self.cascade_model_to_list(model)
+        data = image_to_cascade_data(img)
         try:
+            error_view.setText("")
             for o in operations:
                 data = name_to_function[o[0]](data,*o[1:])
-                if o[0]=="crop":
-                    img = data
-            if type(data)==type(np.array(1)) and data.ndim == 2:
-                self.ui.main_plot.update(color_plot(data))
-            else:
-                self.ui.main_plot.update(color_plot(img, [data]))
 
-            self.ui.error_2d_cascade.setText("")
+            plot = None
+            slope_intercept = None
+            background_image = img
+            if "houghlines" in data:
+                slope_intercept = data["houghlines"]
+            if "image_array" in data:
+                background_image = data["image_array"]
+            if "1d_array" in data:
+                plot = data["1d_array"]
+            if self.use_cropped and "cropped_original" in data:
+                background_image = data["cropped_original"]
+            if "columnwise" in data:
+                measurements = ""
+                measurements += "Column number \t\t|\t Polymer level\n"
+                for i, x in enumerate(data["columnwise"]):
+                    measurements += "Column " + str(i) + "\t\t|\t" + str(x) + " μm\n"
+                error_view.setText(measurements)
+
+
+            self.ui.main_plot.update(color_plot(background_image, plot = plot, slope_intercept = slope_intercept))
+
         except Exception as e:
-            self.ui.error_2d_cascade.setText(str(e))
+            error_view.setText(str(e))
 
 
     def cascade_model_to_list(self, model):
@@ -159,7 +302,8 @@ class MainApp(QWidget):
     def load_cascade_model(self, model, filename):
         with open(filename, "rb") as f:
             cascade = pickle.load(f)
-            print(cascade)
+            for y in range(model.rowCount()):
+                model.removeRows(y,1)
             for row in cascade:
                 self.add_to_cascade_model(model, *row)
 
@@ -172,22 +316,42 @@ class MainApp(QWidget):
         self.add_to_cascade_model(model,*parameters)
 
     def next_image(self):
-        if self.idx_image+1 == len(self.files):
+        self.set_current_index(self.idx_image+1)
+
+
+    def set_current_index(self, idx):
+        if idx < 0 or idx >= len(self.files):
+            print("no such index")
             return
-        self.idx_image += 1
+        self.remember_attributions(1)
+        self.remember_attributions(2)
+        self.idx_image = idx
+        self.ui.file_number.setValue(idx)
         self.show_image()
+        self.update_model_via_attributions(1)
+        self.update_model_via_attributions(2)
+
 
     def previous_image(self):
-        if self.idx_image == 0:
-            return
-        self.idx_image -= 1
-        self.show_image()
+        self.set_current_index(self.idx_image-1)
+
 
     def show_image(self):
-        img = Image.open(self.files[self.idx_image])
+        try:
+            path =  self.files[self.idx_image]
+            img = Image.open(path)
+        except:
+            return
+        img = img.convert('RGBA')
+
+        name =  path.split("/")[-1] #set filename
+        if "tempfiles" in path:
+            name =  path.split("/")[-2] + "/" + path.split("/")[-1]
+        self.ui.filenames.setText(path)
+
         if str(img.mode) != "RGBA":
             img = img.convert('RGB')
-        img = np.array(img,dtype=np.byte)
+        img = color_plot(np.array(img.convert('I'), dtype=np.double))#np.array(img,dtype=np.byte)
         self.ui.main_plot.update(img)
 
     def eventFilter(self, source, event):
@@ -206,15 +370,15 @@ class MainApp(QWidget):
                 self.previous_image()
         return self.widget_handled.eventFilter(source, event)#forward event
 
-    def get_cascade_model(self):
+    def get_cascade_model(self, view, combobox):
         model = QStandardItemModel(0, 5, self)
         model.setHeaderData(0, Qt.Horizontal, "Function name")
         for i in range(1,5):
             model.setHeaderData(i, Qt.Horizontal, "param " + str(i))
-        self.ui.view_2d_cascade.setModel(model)
+        view.setModel(model)
 
         for function in name_to_function:
-            self.ui.combo_box_2d_cascade.addItem(function)
+            combobox.addItem(function)
         return model
 
     def add_to_cascade_model(self, model, *strings):
@@ -223,7 +387,7 @@ class MainApp(QWidget):
 
         root = model.invisibleRootItem()#To set flags
         for i, s in enumerate(strings):
-            model.setData(model.index(current_row, i), s)
+            model.setData(model.index(current_row, i), str(s))
             item = root.child(current_row,i)
             item.setFlags( Qt.ItemIsSelectable | Qt.ItemIsEnabled | Qt.ItemIsDragEnabled | Qt.ItemIsEditable)#Allow only top level drop
 
@@ -236,8 +400,8 @@ class MainApp(QWidget):
 
 class Kapilar():
     def __init__(self):
-        app = QtWidgets.QApplication(sys.argv)
-        self.set_color_theme(app, "light")
+        self.app = QtWidgets.QApplication(sys.argv)
+        self.set_color_theme(self.app, "light")
 
         MainWindow = QtWidgets.QMainWindow()#Create a window
         self.ui = Ui_kapilar()#Instanciate our UI
@@ -252,16 +416,28 @@ class Kapilar():
         self.make_connections()
         MainWindow.show()#and we show it directly
 
-        app.exec_()
+        self.app.exec_()
         self.main_app.shut_down()
-        sys.exit()
+        #sys.exit()
+
+    def toggle_color_scheme(self):
+        app = QApplication.instance()
+        if app is None:
+            raise RuntimeError("No Qt Application found.")
+        if self.use_light:
+            self.set_color_theme(app,"dark")
+        else:
+            self.set_color_theme(app,"light")
+
 
     def set_color_theme(self,app, color):
         path = ""
         if color == "dark":
             path += ":/dark.qss"
+            self.use_light = False
         elif color == "light":
             path += ":/light.qss"
+            self.use_light = True
         else:
             return
         file = QFile(path)
@@ -273,6 +449,7 @@ class Kapilar():
         self.ui.open_file.triggered.connect(self.source_dir_opener.get_folder_path)
         self.ui.exit.triggered.connect(self.main_app.shut_down)
         self.source_dir_opener.folder.connect(self.main_app.load_files)
+        self.ui.toggle_colors.triggered.connect(self.toggle_color_scheme)
 
 if __name__ == "__main__":
     k = Kapilar()
